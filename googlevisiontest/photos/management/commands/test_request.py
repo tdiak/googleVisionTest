9f68273c8aac7165aa03932a9a5804809104aac2
Tomek Diakow <tomek.diakow@gmail.com>
2017-11-10 01:11:54 +0100

    Add vision APi

---
+++ googlevisiontest/photos/management/commands/test_request.py
@@ -0,0 +1,26 @@
+import os
+import io
+
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from google.cloud import vision
+
+
+class Command(BaseCommand):
+    help = 'Send test request'
+    vision_client = vision.Client(settings.API_KEY_PATH)
+
+    def handle(self, *args, **options):
+        file_name = os.path.join(settings.BASE_DIR, 'google', 'test.png')
+
+        with io.open(file_name, 'rb') as image_file:
+            content = image_file.read()
+            image = self.vision_client.image(
+                content=content)
+
+        labels = image.detect_labels()
+
+        print('Labels:')
+        for label in labels:
+            print(label.description)
+        import pdb; pdb.set_trace()
---
+++ googlevisiontest/utils/vision_service.py
@@ -0,0 +1,49 @@
+import os
+import io
+
+from django.conf import settings
+from google.cloud import vision
+
+
+class VisioService(object):
+    vision_client = vision.Client(settings.API_KEY_PATH)
+
+    def __init__(self, image_path, auth_key=None):
+        if auth_key:
+            self.vision_client = vision.Client(settings.API_KEY_PATH)
+
+        self.image_path = image_path
+
+    def __get_labels(self):
+        labels = self.image_data.detect_labels()
+        return [label.description for label in labels]
+
+    def __get_colors(self):
+        colors = self.image_data.detect_properties().colors
+        return [(color.color.red, color.color.green, color.color.blue) for color in colors]
+
+    def __get_faces(self):
+        faces = self.image_data.detect_faces()
+        emotions = []
+        for face in faces:
+            temp = {
+                'joy': face.emotions.joy,
+                'sorrow': face.emotions.sorrow,
+                'anger': face.emotions.anger,
+                'surprise': face.emotions.surprise
+            }
+            emotions.append(temp)
+        return emotions
+
+    def initialize(self):
+        with io.open(self.image_path, 'rb') as image_file:
+            content = image_file.read()
+            self.image_data = self.vision_client.image(
+                content=content)
+
+    def get_info(self):
+        return{
+            'labels': self.__get_labels(),
+            'colors': self.__get_colors(),
+            'faces': self.__get_faces()
+        }
