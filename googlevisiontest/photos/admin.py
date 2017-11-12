650ddb161a0310c1d764965f438a1229bc5c8d17
Tomek Diakow <tomek.diakow@gmail.com>
2017-11-12 20:52:29 +0100

    Add 'Detect one again' via admin

--- googlevisiontest/photos/admin.py
+++ googlevisiontest/photos/admin.py
@@ -3,6 +3,15 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from .models import Photo, Label, Emotion, Color
+from photos.tasks import detect
+
+
+def detect_image(modeladmin, request, queryset):
+    for item in queryset:
+        detect.delay(pk=item.pk)
+
+
+detect_image.short_description = "Detect once again"
 
 
 class LabelInline(admin.TabularInline):
@@ -28,4 +37,8 @@ class PhotoAdmin(admin.ModelAdmin):
         'filename',
         'is_deleted',
         'is_checked'
+    )
+
+    actions = (
+        detect_image,
     )
